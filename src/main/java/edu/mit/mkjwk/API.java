5aa58b5f867c09ab6df4e513aa681a0f32ee75d3
Justin Richer <jricher@mit.edu>
2014-10-25 22:09:52 -0400

    initial project checkin, springboot app

---
+++ src/main/java/edu/mit/mkjwk/API.java
@@ -0,0 +1,5 @@
+package edu.mit.mkjwk;
+
+public class API {
+
+}
---
+++ src/main/java/edu/mit/mkjwk/Application.java
@@ -0,0 +1,19 @@
+package edu.mit.mkjwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
